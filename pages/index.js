6b9d89f3f2fbddd900c9bf110e6f529fbc6aaf8e
Christian Ipanaque <chris.ipanaque@gmail.com>
2022-06-24 10:23:00 -0700

    complete first version of recycling junk removal website

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
       <Head>
         <title>Recycling Junk Removal</title>
         <meta name="description" content="located in Shoreline, Washington" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <header className={styles.header}>
@@ -32,36 +31,6 @@ export default function Home() {
         <p className={styles.description}>
           Our mission is to help our planet by reducing, reusing and recycling.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
